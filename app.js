148b021f7179a909ecde0c4ac64c94ebfd9c048a
FrankGrillo1991 <fsg12491@aol.com>
2024-01-07 03:21:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ question.forEach((element, elementIndex) => {
     element.addEventListener('click', () => {
         let image = question[elementIndex].children[0].children[1];
         question[elementIndex].children[1].classList.toggle('hide');
-        if (image.getAttribute('src') == 'faq-accordion-main\assets\images\icon-plus.svg') {
-            image.setAttribute('src', 'faq-accordion-main\assets\images\icon-minus.svg');
+        if (image.getAttribute('src') == 'faq-accordion-main/assets/images/icon-plus.svg') {
+            image.setAttribute('src', 'faq-accordion-main/assets/images/icon-minus.svg');
         } else {
-            image.setAttribute('src', 'faq-accordion-main\assets\images\icon-plus.svg');
+            image.setAttribute('src', 'faq-accordion-main/assets/images/icon-plus.svg');
         }
     })
 })
